b741b8f28e0b8c384febab2a9074e91096eb2d6b
furkan <furkanmumcu2@gmail.com>
2020-06-07 02:17:50 +0300

    update

--- app.js
+++ app.js
@@ -1,61 +1,124 @@
-
 const cheerio = require('cheerio');
 const axios = require('axios');
 const express = require('express');
 const app = express();
-
+const fs = require('fs');
 
 app.get('/', function (req, res) {
-
-	console.log(req.get('API_TOKEN'))
-	console.log(process.env.API_TOKEN)
-
-	if(req.get('API_TOKEN') && req.get('API_TOKEN') == process.env.API_TOKEN) {
-		axios.get('http://www.kap.org.tr/tr/Sektorler').then(resp => {
-
-			//inject jquery
-			var $ = cheerio.load(resp.data);
-			
-
-			//bilisim sektorunde bulunan sirket sayisi
-			let number = $('#011000a011000a001000a').next().text().match(/\d+/)[0];
-			console.log("bilisim sektorunde bulunan sirket sayisi: " + number);
-
-
-			/**
-			 * replace(/(^[ \t]*\n)/gm, "") -> bos satirlari kaldirir
-			 * replace(/^ +/gm, '') -> satir basi bosluklari kaldirir
-			 * 
-			 * sirket bilgilerini alip array haline getir
-			 */
-			let companiees = $('#011000a011000a001000a').next().next().text().replace(/(^[ \t]*\n)/gm, "").replace(/^ +/gm, '');
-			console.log("bulunan sirketlerin bilgileri:\n" + companiees);
-			let arr = companiees.split("\n");
-			
-
-			//sirket bilgilerini sirket objesine parse edip JSON halinde yazdirma
-			let data = [];
-			for(let i = 3; i<=number*3; i=i+3){
-				let sirket  = new Sirket(arr[i], arr[i+1], arr[i+2]);
-				data.push(sirket);
-			}
-			console.log(JSON.stringify(data));
-			res.send(JSON.stringify(data));
-		});
+	if(!checkToken(req)) {
+		console.log("ok!");
+		res.send("ok!");
+		checkBorsa();
 	}
 	else{
 		res.send("git burdan :(");
 	}
-	
 });
 
 const PORT = process.env.PORT || 8888;
+console.log("listening on port: " + PORT);
 app.listen(PORT);
 
+function checkBorsa(){
+	axios.get('http://www.kap.org.tr/tr/Sektorler').then(resp => {
+
+		//inject jquery
+		var $ = cheerio.load(resp.data);
+				
+		/**
+		 * replace(/(^[ \t]*\n)/gm, "") -> bos satirlari kaldirir
+		 * replace(/^ +/gm, '') -> satir basi bosluklari kaldirir
+		 * 
+		 * sirket bilgilerini alip array haline getir
+		 */
+		let companies = $('#011000a011000a001000a').next().next().text();
+		if(companies === undefined || companies === null || companies === ""){
+			//send error message to client then exit
+			console.log("sending error")
+			return;
+		}
+		companies = companies.replace(/(^[ \t]*\n)/gm, "").replace(/^ +/gm, '');
+		//console.log("bulunan sirketlerin bilgileri:\n" + companies);
+		let arr = companies.split("\n");
+		
+
+		//bilisim sektorunde bulunan sirket sayisi
+		let number = $('#011000a011000a001000a').next().text().match(/\d+/)[0];
+		console.log("bilisim sektorunde bulunan sirket sayisi: " + number);
+
+		//sirket bilgilerini sirket objesine parse edip JSON halinde yazdirma
+		let sirketler = [];
+		let sirketKodlari = [];
+		for(let i = 3; i<=number*3; i=i+3){
+			let sirket  = new Sirket(arr[i], arr[i+1], arr[i+2]);
+			sirketKodlari.push(arr[i+1]);
+			sirketler.push(sirket);
+		}
+		//console.log(JSON.stringify(sirketler));
+		//console.log(JSON.stringify(sirketKodlari));
+
+		compareBorsaData(sirketler, sirketKodlari);
+
+	});
+}
+
+
+function checkToken(req){
+	return req.get('API_TOKEN') && req.get('API_TOKEN') == process.env.API_TOKEN;
+}
+
 class Sirket {
-	constructor(sira,kod, unvan) {
+	constructor(sira, kod, unvan) {
 		this.sira = sira;
 		this.kod = kod;
 		this.unvan = unvan;
 	}
+};
+
+function writeJsonFile(fileName, content){
+	return new Promise(function(resolve,reject){
+		fs.writeFile("./" + fileName + ".json", JSON.stringify(content), function(err) {
+			if(err) {
+				return console.log(err);
+				reject();
+			}
+			resolve();
+		}); 
+	})
+};
+
+function readJsonFile(fileName){
+	return new Promise(function(resolve,reject){
+		fs.readFile("./" + fileName + ".json",  "utf8", function(err, data) {
+			if(err) {
+				return console.log(err);
+				reject();
+			}
+			resolve(JSON.parse(data));
+		}); 
+	})
+};
+
+async function testingLogic(fileName, content){
+	await writeJsonFile(fileName, content);
+	let response = await readJsonFile(fileName);
+	//console.log("response: " + response);
+};
+
+async function compareBorsaData(sirketler, sirketKodlari){
+	//readJsonFile ile eskisirketleri ve eskisirketkodlarini al
+	let oldSirketler = await readJsonFile("sirketler");
+	let oldsirketKodlari = await readJsonFile("sirketKodlari");
+
+	//eski sirketkodlari ile yenisini karsilastir
+	let yeniSirketler = sirketKodlari.filter(x => !oldsirketKodlari.includes(x));
+	let cikanSirketler = oldsirketKodlari.filter(x => !sirketKodlari.includes(x));
+
+	console.log("yeniSirketler: " + yeniSirketler + " cikanSirketler: " + cikanSirketler);
+	//firebase e mesaj yolla
+
+
+	//writeJsonFile ile sirketler ve sirketKodlarini tekrar yaz
+	await writeJsonFile("sirketler", sirketler);
+	await writeJsonFile("sirketKodlari", sirketKodlari);
 };
